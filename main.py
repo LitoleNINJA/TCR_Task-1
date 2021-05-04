4ada32d0bc17a02b7450a32c5cd51d60824af06b
LitoleNINJA <sritwik2@gmail.com>
2021-05-04 20:48:42 +0530

    updated to stream single frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask import Response
-from flask import redirect
 from flask import render_template
 from vision import camera
 
@@ -10,24 +9,14 @@ app = Flask(__name__)
 def index() :
     return render_template("index.html")
 
-cor = None
-percent = None
 def gen(camera) :
     while True :
-        global cor
-        global percent
-        frame, cor, percent = camera.gen_frame()
+        frame = camera.gen_frame()
         yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-
 
 @app.route("/video_feed")
 def video_feed() :
     return Response(gen(camera()), mimetype="multipart/x-mixed-replace; boundary=frame")
 
-@app.route("/data")
-def data() :
-    str(percent)
-    return render_template("info.html", cor=cor, percent=percent)
-
 if __name__ == '__main__' :
-    app.run(debug = True)
+    app.run(debug=True)
