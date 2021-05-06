4874e980cc9dd136bdb02fe2594618fc59893d11
LitoleNINJA <sritwik2@gmail.com>
2021-05-06 18:48:32 +0530

    updated to send data to html file

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from flask import Flask
-from flask import Response
-from flask import render_template
+from flask import Flask, Response, render_template, jsonify
 from ball_tracking import camera
 
 app = Flask(__name__)
@@ -9,14 +7,31 @@ app = Flask(__name__)
 def index() :
     return render_template("index.html")
 
+corner= "None"
+percent_vol = "None"
 def gen(camera) :
     while True :
-        frame = camera.gen_frame()
+        global corner
+        global percent_vol
+        frame, corner, percent_vol = camera.gen_frame()
         yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 
 @app.route("/video_feed")
 def video_feed() :
     return Response(gen(camera()), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+@app.route("/send_data")
+def send_data() :
+    global percent_vol
+    print(corner, percent_vol)
+    if percent_vol == '0.0' :
+        percent_vol = "None"
+    percent_vol += " %"
+    ball = "Not Present"
+    if corner != "None" :
+        ball = "Present" 
+    return jsonify(ball=ball, corner=corner, area=percent_vol)
 
 if __name__ == '__main__' :
     app.run(debug=True)
